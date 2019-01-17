814952d6edfd47a2f6685e2cacebbfd9105a2089
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-01-17 10:45:46 +0330

    1

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,6 @@ class Upload < ActiveRecord::Base
   has_attached_file :attachment, :styles => { :medium => "300x300#", :tiny => "20x20>" ,:thumb => "50x50>", :large => "400x400>", :extra => "800X800>"  }, :default_url => "/assets/noimage-35-:style.jpg",  :processors => [:cropper]
   validates_attachment_content_type :attachment, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  do_not_validate_attachment_file_name :attachment
   belongs_to :uploadable, :polymorphic => true
   belongs_to :advertisement, :class_name => "Advertisement", :foreign_key => "uploadable_id"
 
