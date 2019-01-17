2615691843087cfdd7ba818d89cc6efcd9eeb5bc
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-01-17 10:20:47 +0330

    1

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -21,29 +21,42 @@ class ApiController < ApplicationController
   end
 
 
-    def sign_up
-      @user = User.new(username: params['username'], mobile: params['username'], password: params['password'], password_confirmation: params['password_confirmation'])
-      if @user.save
-        @profile = Profile.create(user_id: @user.id, name: params[:name])
-        render :json => {result: 'OK', token: JWTWrapper.encode({ user_id: @user.id })}.to_json, :callback => params['callback']
-      else
-        render :json => {result: 'ERROR', error: @user.errors }.to_json , :callback => params['callback']
-      end
+  def sign_up
+    @user = User.new(username: params['username'], mobile: params['username'], password: params['password'], password_confirmation: params['password_confirmation'])
+    if @user.save
+      @profile = Profile.create(user_id: @user.id, name: params[:name])
+      render :json => {result: 'OK', token: JWTWrapper.encode({ user_id: @user.id })}.to_json, :callback => params['callback']
+    else
+      render :json => {result: 'ERROR', error: @user.errors }.to_json , :callback => params['callback']
     end
+  end
 
-    def advertisement
-      @advertisement = Advertisement.find(params[:id])
-      @photos = []
-      for photo in @advertisement.photos('large')
-        @photos << {url:  request.base_url + photo[:url], id: photo[:id]}
-      end
-      @result = {id: @advertisement.id, title: @advertisement.title, content: @advertisement.content,'cover' => request.base_url + @advertisement.cover('large'), photos: @photos}
-      render :json => @result.to_json, :callback => params['callback']
+  def advertisement
+    @advertisement = Advertisement.find(params[:id])
+    @photos = []
+    for photo in @advertisement.photos('large')
+      @photos << {url:  request.base_url + photo[:url], id: photo[:id]}
     end
+    @result = {id: @advertisement.id, title: @advertisement.title, content: @advertisement.content,'cover' => request.base_url + @advertisement.cover('large'), photos: @photos}
+    render :json => @result.to_json, :callback => params['callback']
+  end
 
-    def make_advertisement
-      @advertisement = Advertisement.create(title: params[:title])
-      @result = {id: @advertisement.id}
-      render :json => @result.to_json, :callback => params['callback']
+  def make_advertisement
+    @advertisement = Advertisement.create(title: params[:title])
+    @result = {id: @advertisement.id}
+    render :json => @result.to_json, :callback => params['callback']
+  end
+
+  def upload
+    @upload = Upload.new
+    @upload.uploadable_type = params[:uploadable_type]
+    @upload.uploadable_id = params[:uploadable_id]
+    @upload.attachment_type = params[:attachment_type]
+    @upload.attachment = params[:file]
+    if @upload.save
+      render :json => {result: 'OK' }.to_json , :callback => params['callback']
+    else
+      render :json => {error: 'ERROR' }.to_json , :callback => params['callback']
     end
+  end
 end
