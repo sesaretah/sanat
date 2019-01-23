bd06348314865df6eae09f2a545e466c8713b7f1
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-01-23 17:16:34 +0330

    1

--- db/migrate/20190122132638_add_advertisement_id_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddAdvertisementIdToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :advertisement_id, :string
-    add_index :messages, :advertisement_id
-  end
-end
