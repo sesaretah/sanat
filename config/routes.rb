39d2c2283e8cf5dabaccbddf0b800bb4d8ea8b13
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-01-26 14:47:20 +0330

    1

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,8 @@ Rails.application.routes.draw do
   get '/api/user_advert_room/:id', to: "api#user_advert_room"
   get '/api/all_unseens', to: "api#all_unseens"
   get '/api/my_pins', to: "api#my_pins"
+  get '/api/update_token', to: "api#update_token"
+
 
 
   post '/api/sign_up', to: "api#sign_up"
